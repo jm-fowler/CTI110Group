2029edbcb9823c4a110b2b0e0c03691d9148b76e
Justin Fowler <133838530+jm-fowler@users.noreply.github.com>
2023-12-10 14:16:45 -0500

    Update about.html, home.html, and user_page.php

--- user_page.php
+++ user_page.php
@@ -1,117 +1,153 @@
 <?php
     session_start();
-    if(!isset($_SESSION['logged_in'])) {
-        header('LOCATION:login.php');
+    if (!isset($_SESSION['logged_in'])) {
+        header('LOCATION: login.php');
         die();
     }
+    $servername = "hermes.waketech.edu";
+    $username = "jdiveris";
+    $dbname = "test";
+    $mysql_password = "csc124";
+
+    $conn = mysqli_connect($servername, $username, $mysql_password, $dbname);
+    if ($conn->connect_error) {
+        die("\nConnection failed: " . mysqli_connect_error());
+    }
+    $user_id = $_SESSION["user_id"];
+    $sql = "UPDATE orders SET active = 'FALSE' WHERE end_date < CURDATE() AND user_id = '$user_id';";
+    mysqli_query($conn, $sql);
+    $sql = "SELECT order_id, subscription_id, end_date, active FROM orders WHERE user_id = '$user_id' ORDER BY order_id DESC LIMIT 1;";
+    $result = mysqli_query($conn, $sql);
+    $row = $result->fetch_array(MYSQLI_NUM);
+    $order_id = $row[0];
+    $subscription_id = $row[1];
+    $end_date = date('M d, Y', strtotime($row[2]));
+    $active = $row[3];
+    $subscription_desc = [
+        1 => "Standard - 1 Month Subscription",
+        2 => "Standard - 6 Month Subscription",
+        3 => "Standard - 1 Year Subscription",
+        4 => "Plus - 1 Month Subscription",
+        5 => "Plus - 6 Month Subscription",
+        6 => "Plus - 1 Year Subscription",
+        7 => "Premium - 1 Month Subscription",
+        8 => "Premium - 6 Month Subscription",
+        9 => "Premium - 1 Year Subscription"
+    ];
+    $sql = "SELECT last_name, first_name, email, phone_num FROM users WHERE user_id = '$user_id';";
+    $result = mysqli_query($conn, $sql);
+    $row = $result->fetch_array(MYSQLI_NUM);
+
+    $last_name = $row[0];
+    $first_name = $row[1];
+    $email = $row[2];
+    $phone_num = $row[3];
 ?>
-
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>User Page</title>
-        <link rel="stylesheet" type="text/css" href="style.css" />
-    </head>
-    <nav>
-        <ul>
-            <li><a href="home.php">Home</a>
-            <li><a href="logout.php">Logout</a>
-            <li><a href="about.html" target="_blank">About RetroStream</a>
-        </ul>
-    </nav>
-
-    <?php
-
-        $user_id = $_SESSION["user_id"];
-        $servername = "hermes.waketech.edu";
-        $username = "jdiveris";
-        $dbname = "test";
-        $mysql_password = "csc124";
-        $error = 0;
-        
-        $conn = mysqli_connect($servername, $username, $mysql_password, $dbname);
-        if ($conn->connect_error){
-            die("\nConnection failed: " . mysqli_connect_error());
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        body {
+            background-color: lightblue;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
         }
 
-        $sql = "UPDATE orders SET active = 'FALSE' WHERE end_date < CURDATE() AND user_id = '$user_id';";
-        mysqli_query($conn, $sql);
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: lightblue;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+        }
 
-        $sql = "SELECT order_id, subscription_id, end_date, active FROM orders WHERE user_id = '$user_id' ORDER BY order_id DESC LIMIT 1;";
-        $result = mysqli_query($conn, $sql);
-        
- 
-            $row = $result->fetch_array(MYSQLI_NUM);
-            $order_id = $row[0];
-            $subscription_id = $row[1];
-            $end_date = $row[2];
-            $active = $row[3];
-            
-            $end_date = date('M d, Y', (strtotime($end_date)));
+        .user-info,
+        .subscription-info {
+            margin-top: 20px;
+        }
 
-            if ($subscription_id == 1){
-                $subscription_desc = "Standard - 1 Month Subscription";
-            }elseif ($subscription_id == 2){
-                $subscription_desc = "Standard - 6 Month Subscripton";
-            }elseif ($subscription_id == 3){
-                $subscription_desc = "Standard - 1 Year Subscription";
-            }elseif ($subscription_id == 4){
-                $subscription_desc = "Plus - 1 Month Subscription";
-            }elseif ($subscription_id == 5){
-                $subscription_desc = "Plus - 6 Month Subscripton";
-            }elseif ($subscription_id == 6){
-                $subscription_desc = "Plus - 1 Year Subscription";
-            }elseif ($subscription_id == 7){
-                $subscription_desc = "Premium - 1 Month Subscription";
-            }elseif ($subscription_id == 8){
-                $subscription_desc = "Premium - 6 Month Subscripton";
-            }elseif ($subscription_id == 9){
-                $subscription_desc = "Premium - 1 Year Subscription";
-            }
+        .user-info p,
+        .subscription-info p {
+            margin: 5px 0;
+        }
 
-            $sql = "SELECT last_name, first_name, email, phone_num FROM users WHERE user_id = '$user_id';";
-            $result = mysqli_query($conn, $sql);
-            $row = $result->fetch_array(MYSQLI_NUM);
+        .subscription-info {
+            background-color: #fff;
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+        }
 
-            $last_name = $row[0];
-            $first_name = $row[1];
-            $email = $row[2];
-            $phone_num = $row[3];
+        nav {
+            background-color: #333;
+            color: white;
+            padding: 10px;
+            text-align: center;
+        }
 
-    ?>
-    <body>
-        <?php
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
 
+        nav ul li {
+            display: inline;
+            margin-right: 15px;
+        }
 
-                echo "<p>$first_name</p>";
-                echo "<p>$last_name</p>";
-                echo "<p>$email</p>";
-                echo "<p>$phone_num</p>";
-           
-                echo $user_id;
-        
+        nav a {
+            text-decoration: none;
+            color: white;
+            font-weight: bold;
+            font-size: 16px;
+            transition: color 0.3s ease;
+        }
 
-        ?>
+        nav a:hover {
+            color: #ffcc00;
+        }
+    </style>
+</head>
+<body>
 
-        <h2><center>Stand-in page for testing</center></h2>
-        <br><br>
-
-        <?php
-
-
-            if ($active == "TRUE"){
-                echo "<p><b>$subscription_desc</b> | Status : ACTIVE Until $end_date </p>";
-            }elseif ($active == "FALSE"){
-                echo "<p><b>$subscription_desc</b> | Status : INACTIVE Since $end_date </p>";
-            }
-
-
-        ?>
-
+    <nav>
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="logout.php">Logout</a></li>
+            <li><a href="about.html" target="_blank">About RetroStream</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <h2>User Information</h2>
+        <div class="user-info">
+            <?php
+                echo "<p><strong>Name:</strong> $first_name $last_name</p>";
+                echo "<p><strong>Email:</strong> $email</p>";
+                echo "<p><strong>Phone Number:</strong> $phone_num</p>";
+                echo "<p><strong>User ID:</strong> $user_id</p>";
+            ?>
+        </div>
+        <div class="subscription-info">
+            <?php
+                if ($active == "TRUE") {
+                    echo "<p><strong>Subscription:</strong> {$subscription_desc[$subscription_id]}</p>";
+                    echo "<p><strong>Status:</strong> ACTIVE Until $end_date </p>";
+                } elseif ($active == "FALSE") {
+                    echo "<p><strong>Subscription:</strong> {$subscription_desc[$subscription_id]}</p>";
+                    echo "<p><strong>Status:</strong> INACTIVE Since $end_date </p>";
+                }
+            ?>
+        </div>
         <a href="subscription_management.php">Manage Subscriptions</a>
-
-    </body>
+    </div>
     <footer>
         <p>This page was created by .... </p>
     </footer>
+</body>
 </html>
